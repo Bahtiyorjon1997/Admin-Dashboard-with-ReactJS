9d3cefc4ac8bf6939919ffb828fc71d41a250a9f
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2021-10-06 00:06:09 +0500

    icons placed

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,20 +1,26 @@
 import React from "react";
 import "./Topbar.css";
 import { NotificationsNone } from "@material-ui/icons";
+import LanguageIcon from "@material-ui/icons/Language";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 const Topbar = () => {
   return (
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="top-left">
-          <span className="logo">
-            <pre>lamaData</pre>
-          </span>
+          <span className="logo">lamaData</span>
         </div>
         <div className="top-right">
           <div className="topBarIconContainer">
             <NotificationsNone />
             <span className="topIconBadge">2</span>
+          </div>
+          <div className="topBarIconContainer">
+            <LanguageIcon />
+          </div>
+          <div className="topBarIconContainer">
+            <SettingsIcon />
           </div>
         </div>
       </div>
