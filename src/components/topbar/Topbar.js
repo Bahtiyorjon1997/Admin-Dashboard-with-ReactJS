1d7ca7829bbd5c8f1d979200e33c49386d6e5ca9
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2021-10-05 17:30:23 +0500

    logo fixed

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Topbar.css";
 
 const Topbar = () => {
-  return <div>Topbar</div>;
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="top-right">
+          <span className="logo">lamaData</span>
+        </div>
+        <div className="top-left">Left</div>
+      </div>
+    </div>
+  );
 };
 
 export default Topbar;
