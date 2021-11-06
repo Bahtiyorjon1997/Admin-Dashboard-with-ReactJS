ad0b4b513d8bb4ded2889d00c5ed3a6fc8f09d6f
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2021-11-06 17:37:23 +0500

    newest

--- src/pages/manage/Manage.js
+++ src/pages/manage/Manage.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Manage.css";
 
 const Manage = () => {
-  return <div className="manage"></div>;
+  return <div className="manage">Manage</div>;
 };
 
 export default Manage;
